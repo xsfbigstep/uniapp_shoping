1695b61e84b5066cd9560cdf3437493e8fc83545
xiaosf <835681612@qq.com>
2022-08-12 17:49:26 +0800

    新建uniapp uniapp-ui版 并实现tablist标签切换功能

---
+++ pages/category/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		分类
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="index">
+		首页
+		<view class="iconfont icon-gouwucheman"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.index {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
